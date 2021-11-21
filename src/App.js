95a3ef049c667f121bb5aaa30e09fc45a79ec1d4
ja2z <93806968+ja2z@users.noreply.github.com>
2021-11-20 23:49:52 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,92 @@
-import logo from './logo.svg';
-import './App.css';
+import { useMemo, useRef } from "react";
+import Highcharts from "highcharts";
+import wordCloud from "highcharts/modules/wordcloud.js";
+import HighchartsReact from "highcharts-react-official";
+import { client, useConfig, useElementData } from "@sigmacomputing/plugin";
+//npm install highcharts highcharts-react-official
+
+client.config.configureEditorPanel([
+  { name: "source", type: "element" },
+  { name: "dimension", type: "column", source: "source", allowMultiple: false },
+  { name: "measures", type: "column", source: "source", allowMultiple: false },
+  { name: "Tokenize y/n?", type: "text", defaultValue: "N" },
+]);
+
+wordCloud(Highcharts);
+
+Highcharts.seriesTypes.wordcloud.prototype.deriveFontSize = function (
+  relativeWeight
+) {
+  var maxFontSize = 25;
+  // Will return a fontSize between 0px and 25px.
+  return Math.floor(maxFontSize * relativeWeight);
+};
 
 function App() {
+  const config = useConfig();
+  const sigmaData = useElementData(config.source);
+  const ref = useRef();
+  const options = useMemo(() => {
+    const dimensions = config.dimension;
+    const measures = config.measures;
+
+    //transform sigmaData --> wordcloud data
+    if (sigmaData?.[dimensions]) {
+      let dataMap = [];
+      let sw = require("stopword");
+      for (let i = 0; i < sigmaData[dimensions].length; i++) {
+        const text = sigmaData[dimensions][i];
+        let weight = 1;
+        // if a measure is specified, use it for the weighting
+        if (sigmaData?.[measures]) {
+          weight = sigmaData[measures][i];
+        }
+        // add text & weighting to dataMap, tokenize optional
+        if (client.config.getKey("Tokenize y/n?") === "Y") {
+          const words = sw.removeStopwords(text.split(/[,. ]+/g));
+          for (let j = 0; j < words.length; j++) {
+            dataMap[words[j]] === undefined
+              ? (dataMap[words[j]] = weight)
+              : (dataMap[words[j]] += weight);
+          }
+        } else {
+          dataMap[text] === undefined
+            ? (dataMap[text] = weight)
+            : (dataMap[text] += weight);
+        }
+      }
+
+      // convert dataMap to array
+      let data = [];
+      let i = 0;
+      for (var key in dataMap) {
+        data[i] = { name: key, weight: dataMap[key] };
+        i++;
+      }
+
+      const options = {
+        series: [
+          {
+            type: "wordcloud",
+            data: data,
+          },
+        ],
+        title: {
+          text: undefined,
+        },
+        chart: {
+          height: (9 / 16) * 100 + "%",
+          //backgroundColor: "transparent",
+        },
+      };
+
+      return options;
+    }
+  }, [config, sigmaData]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <HighchartsReact highcharts={Highcharts} options={options} ref={ref} />
     </div>
   );
 }
